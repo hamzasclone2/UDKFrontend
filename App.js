c0c87f4cb5c55fc7efff8694bb31620b5c9c26a8
Ethan Malin <ethanmalin01@gmail.com>
2019-03-29 14:00:02 -0500

    modularized story screen

--- App.js
+++ App.js
@@ -5,6 +5,8 @@ import { StyleSheet, Text, View, Image, TextInput, Button, Alert,
 TouchableHighlight, TouchableOpacity, TouchableNativeFeedback,
 TouchableWithoutFeedback, ScrollView, FlatList } from 'react-native';
 
+import StoryScreen from './story';
+
 import {createStackNavigator, createAppContainer, createBottomTabNavigator} from 'react-navigation';
 import { List, ListItem, SearchBar } from "react-native-elements";
 
@@ -26,7 +28,7 @@ class HomeScreen extends React.Component {
     }
 
     async asyncData() {
-        let response = await axios.get("http://104.248.235.9:3001/api");
+        let response = await axios.get("http://104.248.235.9:3001/api/top");
         list = response.data
         return this.setState(
             { loading: false, data: list }
@@ -34,9 +36,10 @@ class HomeScreen extends React.Component {
     }
 
     renderCard = (item) => {
+        console.log(item)
         return (
             <TouchableOpacity style={styles.Card}
-                onPress={() => this.props.navigation.navigate('Story')}>    
+                onPress={() => this.props.navigation.navigate('Story', item)}>    
                 <ListItem title={item.headline}/>
             </TouchableOpacity>
         );
@@ -56,29 +59,7 @@ class HomeScreen extends React.Component {
     }
 }
 
-//A page for a default story
-class StoryScreen extends React.Component {
 
-    render(){
-        return (
-            <ScrollView style={{backgroundColor: '#D3D3D3'}}>
-                <View style ={{
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    margin:5
-                }}>
-                <Text style={{fontSize:18}}>{'Kansas overwhelms short-handed West Virginia roster in 78-53 victory'}</Text>
-                <Text style={{fontSize:10}}>Maddy Tannahill | @maddytannahill   Feb 16, 2019</Text>
-                <Image source={{uri: "https://bloximages.chicago2.vip.townnews.com/kansan.com/content/tncms/assets/v3/editorial/5/54/55450714-3241-11e9-8ca1-8bc32dfb7918/5c689a8b9c949.image.jpg?resize=750%2C968", width: 230, height: 296}} />
-                <Text style={{fontSize:10}}>Redshirt sophomore guard K.J. Lawson celebrates after hitting a three against West Virginia. The Jayhawks defeated the Mountaineers 78-53 on Saturday, Feb. 16.</Text>
-                <Text style={{fontSize:10, color:"gray"}}>Chance Parker/KANSAN</Text>
-                <Text style={{fontSize:12}}>{'\nWorking with a four-point margin over West Virginia, freshman guard Quentin Grimes drove into a Mountaineer-crowded lane. Immediately double-teamed, the freshman dropped off a nasty no-look pass to junior forward Mitch Lightfoot who brought the roof down in Allen Fieldhouse with a massive dunk, extending the lead to 13-7 for the Jayhawks.'}</Text>
-                <Text style={{fontSize:12}}>{'\nFrom this moment, Kansas went on a 14-2 run and took dominating control over the Mountaineers, resulting in a 78-53 victory for the Jayhawks.'}</Text>
-                </View>
-            </ScrollView>
-        );
-    }
-}
 
 //Allows us to use the UDK logo
 class LogoTitle extends React.Component {
@@ -110,6 +91,7 @@ const AppNavigator = createStackNavigator(
     }
 );
 
+// tabs should only be on home screen, and should probably be a component, we are only changing the data on the home screen, not changing which screen we are on
 //Allows us to have a tabs on the bottom
 export default createAppContainer(createBottomTabNavigator(
     {
@@ -149,11 +131,10 @@ export default createAppContainer(createBottomTabNavigator(
     }
 ));
 
-//Some CSS-like styling for some elements
 const styles = StyleSheet.create({
     Card: {
         borderWidth: 1,
-        borderColor: '#00ff33',
+        borderColor: '#cccccc',
         backgroundColor: '#cccccc',
     },
 });
---
+++ story.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image, ScrollView} from 'react-native';
+
+//A page for a default story
+export default class StoryScreen extends React.Component {
+    constructor(props) {
+        super(props)
+        const {state} = props.navigation;
+        this.data = state.params
+        this.trimDate = (date) => date.substring(0, date.indexOf('T'));
+    }
+
+    render(){
+        return (
+            <ScrollView style={{backgroundColor: '#cccccc'}}>
+                <View style={styles.BodyWrapper}>
+                    <Text style={styles.Headline}>{this.data.headline}</Text>
+                    <Text style={styles.Byline}>{this.data.author} | {this.trimDate(this.data.date)}</Text>
+                    <Image source={{uri: "https://bloximages.chicago2.vip.townnews.com/kansan.com/content/tncms/assets/v3/editorial/5/54/55450714-3241-11e9-8ca1-8bc32dfb7918/5c689a8b9c949.image.jpg?resize=750%2C968", width: 230, height: 296}} />
+                    <Text style={styles.ImageCaption}>Redshirt sophomore guard K.J. Lawson celebrates after hitting a three against West Virginia. The Jayhawks defeated the Mountaineers 78-53 on Saturday, Feb. 16.</Text>
+                    <Text style={styles.ImageCaption}>Chance Parker/KANSAN</Text>
+                    <Text style={styles.Body}>{this.data.body}</Text>
+                </View>
+            </ScrollView>
+        );
+    }
+
+
+}
+
+const styles = StyleSheet.create({
+    BodyWrapper: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin:5,
+    },
+    Headline: {
+        fontSize: 18,
+    },
+    Byline: {
+        fontSize: 10,
+    },
+    MainImage: {
+        resizeMode: 'stretch',
+    },
+    ImageCaption: {
+        fontSize: 10,
+        color: '#777777',
+    },
+    Body: {
+        fontSize: 12,
+    },
+});
