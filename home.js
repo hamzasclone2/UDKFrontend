7ecbe5fc9ac5cb6ff140c06f1d6ce111096a411d
Ethan Malin <ethanmalin01@gmail.com>
2019-04-10 10:11:54 -0500

    fixed search bar crashing by only searching local data

--- home.js
+++ home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import _ from 'lodash';
 import { getData, contains } from './api';
-import { StyleSheet, Text, Image, View, TouchableOpacity, FlatList } from 'react-native';
+import { StyleSheet, Text, Image, View, TouchableOpacity, FlatList, ActivityIndicator } from 'react-native';
 
 import TabNav from './tabnav';
 
@@ -13,10 +13,12 @@ export default class HomeScreen extends React.Component {
         this.tabTouchHandler = this.tabTouchHandler.bind(this)
         this.state = {
             loading: true,
+            numDisplayed: 40,
             displayedData: [],
             serverData: [],
             error: null,
             refreshing: false,
+            searching: false,
             currentTab: 'top',
             search: "",
         };
@@ -25,39 +27,56 @@ export default class HomeScreen extends React.Component {
     }
 
     componentDidMount() {
-        this.loadData("top", "");
+        this.setState( {loading: true} );
+        this.loadData();
     }
 
-    async loadData(category = "top", searchText = "") { // default values
-        let list = await getData(category, this.state.search);
-        this.setState(
-            { 
+    loadData(category = "top", searchText = "") { // default values
+        getData(category, searchText)
+        .then((data) => { 
+            this.setState({
                 loading: false,
-                displayedData: list,
-                serverData: list,
-            }
-        );
+                numDisplayed: 40,
+                displayedData: data.slice(1,40),
+                serverData: data,
+            })
+        })
+        .catch((error) => {
+            console.log('There has been a problem with your fetch operation: ' + error.message);
+        });
     }
 
     tabTouchHandler(tab) {
         if(tab != this.state.currentTab) {
-            getData(tab);
+            this.setState({
+                loading: true,
+            })
+
+            getData(tab)
+            .then((data) => {
+                this.setState({
+                    loading: false,
+                    numDisplayed: 40,
+                    displayedData: data.slice(1,40),
+                    serverData: data,
+                    currentTab: tab,
+                })
+            });
         }
     }
 
     searchHandler = (searchText) => {
-        console.log('search', searchText)
         const newData = _.filter(this.state.serverData, article => {
             return contains(article, searchText, newData)
         } );
 
         this.setState({
             displayedData: newData,
-            search: searchText, newData}, () => this.loadData('search', searchText)); // to get latest query list/state
+            search: searchText, newData
+        });// () => this.loadData('search', searchText)); // to get latest query list/state
     }
 
     renderCard = (item) => {
-        // console.log(item.main_image);
         if(item.main_image == null) {
             return (
                 <TouchableOpacity style={styles.Card}
@@ -79,27 +98,43 @@ export default class HomeScreen extends React.Component {
     }
 
     render() {
-        return (
-            <View style={{flex: 1}}>
-                <View style={{flex: 1.5}}>
-                    <SearchBar
-                        placeholder="Type Here..."
-                        lightTheme
-                        onChangeText={text => this.searchHandler(text)}
-                        onClear={text => this.searchHandler('')}
-                        value={this.state.search}
-                    />
-                    <FlatList
-                        data={this.state.displayedData}
-                        renderItem={({ item }) => this.renderCard(item)}
-                        keyExtractor={(item, index) => (item.id).toString()}
-                    />    
-                </View>  
-                <View style={styles.Tabs}>
-                    <TabNav tabTouchCallback={this.tabTouchHandler}></TabNav>
-                </View>  
-            </View>
-        );
+        if(this.state.loading) {
+            return (
+                <View style={{flex: 1}}>
+                    <View style={{flex: 1.5}}>
+                        <ActivityIndicator style={{marginTop: 20}}
+                            size="large" color="#0000ff"
+                        />    
+                    </View>  
+                    <View style={styles.Tabs}>
+                        <TabNav tabTouchCallback={this.tabTouchHandler}></TabNav>
+                    </View>  
+                </View>     
+            );
+        } else {
+            return (
+                <View style={{flex: 1}}>
+                    <View style={{flex: 1.5}}>
+                        <SearchBar
+                            placeholder="Type Here..."
+                            lightTheme
+                            onChangeText={text => this.searchHandler(text)}
+                            onClear={text => this.searchHandler('')}
+                            value={this.state.search}
+                        />
+                        <FlatList
+                            data={this.state.displayedData}
+                            renderItem={({ item }) => this.renderCard(item)}
+                            keyExtractor={(item, index) => (item.id).toString()}
+                        />    
+                    </View>  
+                    <View style={styles.Tabs}>
+                        <TabNav tabTouchCallback={this.tabTouchHandler}></TabNav>
+                    </View>  
+                </View>
+            );
+        }
+
     }
 }
 
